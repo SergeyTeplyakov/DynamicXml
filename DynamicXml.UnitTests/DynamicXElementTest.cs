b0691af3741aa52c48619371d9e05ec880ab53d0
Sergey Teplyakov <Sergey.Teplyakov@gmail.com>
2011-04-16 21:06:23 +0300

    Add solution to git.

---
+++ DynamicXml.UnitTests/DynamicXElementTest.cs
@@ -0,0 +1,205 @@
+﻿using NUnit.Framework;
+using System;
+using System.Xml.Linq;
+
+namespace DynamicXml.UnitTests
+{
+    /// <summary>
+    /// Unit tests for DynamicXElement class
+    /// </summary>
+    [TestFixture]
+    public class DynamicXElementTest
+    {
+        //---------------------------------------------------------------------------------------//
+        // Construction
+        //---------------------------------------------------------------------------------------//
+        #region Construction
+
+        [TestCase(ExpectedExceptionName = "System.Diagnostics.Contracts.__ContractsRuntime+ContractException")]
+        public void CreateInstanceFailureTest()
+        {
+            dynamic actual = DynamicXElement.CreateInstance(null);
+        }
+
+        [TestCase]
+        public void CreateInstanceTest()
+        {
+            XElement element = new XElement("name", "value");
+            dynamic actual = DynamicXElement.CreateInstance(element);
+            Assert.IsNotNull(actual);
+        }
+        #endregion Construction
+
+        //---------------------------------------------------------------------------------------//
+        // System.Object overrides
+        //---------------------------------------------------------------------------------------//
+        #region System.Object overrides
+
+        // All this methods should delegate calls to underlying element methods
+        
+        [TestCase]
+        public void ToStringMethodTest()
+        {
+            // ToString method should call underlying element.ToString method
+            XElement element = new XElement("name", "value");
+            dynamic dynamicElement = DynamicXElement.CreateInstance(element);
+            string actual = dynamicElement.ToString();
+            string expected = element.ToString();
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [TestCase]
+        public void EqualsMethodTest()
+        {
+            // ToString method should call underlying element.ToString method
+            XElement element1 = new XElement("name", "value");
+            XElement element2 = new XElement("name", "value");
+            dynamic dynamicElement1 = DynamicXElement.CreateInstance(element1);
+            dynamic dynamicElement2 = DynamicXElement.CreateInstance(element2);
+
+            Console.WriteLine("Equals: " + dynamicElement1.Equals(dynamicElement2));
+            Assert.That(element1.Equals(element2), 
+                    Is.EqualTo(dynamicElement1.Equals(dynamicElement2)));
+
+            Console.WriteLine("Hashcodes: element1: {0}, element2: {1}", element1.GetHashCode(), element2.GetHashCode());
+            Assert.That(element1.GetHashCode(), Is.EqualTo(dynamicElement1.GetHashCode()));
+            Assert.That(element2.GetHashCode(), Is.EqualTo(dynamicElement2.GetHashCode()));
+        }
+
+        #endregion System.Object overrides
+
+        //---------------------------------------------------------------------------------------//
+        // Accessing value
+        //---------------------------------------------------------------------------------------//
+
+        #region Accessing value
+
+        [TestCase]
+        public void AccessXElementTest()
+        {
+            // Cast to XElement should lead to obtaining underlying XElement
+            XElement element = new XElement("name", "value");
+            dynamic dynamicElement = DynamicXElement.CreateInstance(element);
+            XElement underlyingElement = dynamicElement;
+            Assert.That(object.ReferenceEquals(underlyingElement, element));
+        }
+
+        [TestCase("name", 1, Result = 1)]
+        public object AccessValueElementTest(string elementName, object value)
+        {
+            XElement element = new XElement("name", "value");
+            dynamic dynamicElement = DynamicXElement.CreateInstance(element);
+            object actual = dynamicElement;
+            return actual;
+        }
+
+        [TestCase]
+        public void AccessStringValueElementTest()
+        {
+            XElement element = new XElement("name", "value");
+            dynamic dynamicElement = DynamicXElement.CreateInstance(element);
+            string actual = dynamicElement;
+            Assert.That(actual, Is.EqualTo("value"));
+            
+            Console.WriteLine("Actual string value: {0}", actual);
+        }
+
+        [TestCase]
+        public void AccessIntValueElementTest()
+        {
+            XElement element = new XElement("name", 1);
+            dynamic dynamicElement = DynamicXElement.CreateInstance(element);
+            int actual = dynamicElement;
+            Assert.That(actual, Is.EqualTo(1));
+
+            Console.WriteLine("Actual int value: {0}", actual);
+        }
+
+        [TestCase]
+        public void AccessDoubleValueElementTest()
+        {
+            XElement element = new XElement("name", 1f);
+            dynamic dynamicElement = DynamicXElement.CreateInstance(element);
+            float actual = dynamicElement;
+            Assert.That(actual, Is.EqualTo(1f));
+
+            Console.WriteLine("Actual float value: {0}", actual);
+        }
+
+        // TODO: We should use more intellectual way for testing the rest of the underlying types
+
+        //---------------------------------------------------------------------------------------//
+        // Accessing value
+        //---------------------------------------------------------------------------------------//
+
+        #region Accessing value
+
+        [TestCase]
+        public void AccessingZeroIndexTest()
+        {
+            XElement element = new XElement("name", "value");
+            dynamic dynamicElement = DynamicXElement.CreateInstance(element);
+            // Access to zero element means access to element itself
+            string value = dynamicElement[0];
+            Assert.That(value, Is.EqualTo("value"));
+
+            Console.WriteLine("Value is: {0}", value);
+        }
+
+        [TestCase]
+        public void AccessingNonZeroIndexTest()
+        {
+            XElement element = new XElement("name", 
+                new XElement("Subelement", "value1"),
+                new XElement("Subelement", "value2"));
+            dynamic dynamicElement = DynamicXElement.CreateInstance(element);
+
+            string value1 = dynamicElement.Subelement[0];
+            Assert.That(value1, Is.EqualTo("value1"));
+            Console.WriteLine("value1 is: {0}", value1);
+
+            string value2 = dynamicElement.Subelement[1];
+            Assert.That(value2, Is.EqualTo("value2"));
+            Console.WriteLine("value2 is: {0}", value2);
+        }
+
+        [TestCase(ExpectedExceptionName = "System.Diagnostics.Contracts.__ContractsRuntime+ContractException")]
+        public void AccessingNonZeroIndexForOneElementTest()
+        {
+            XElement element = new XElement("name", "value");
+            dynamic dynamicElement = DynamicXElement.CreateInstance(element);
+            string value = dynamicElement[1];
+            Assert.Fail();
+        }
+
+        #endregion Accessing value
+
+
+
+        #endregion Accessing value
+
+        //---------------------------------------------------------------------------------------//
+        // Accessing subelements
+        //---------------------------------------------------------------------------------------//
+
+        #region Accessing subelements
+
+        [TestCase]
+        public void AccessStringSubelement()
+        {
+            XElement subelement = new XElement("Subelement", "value");
+            XElement element = new XElement("Element", subelement);
+            dynamic dynamicElement = DynamicXElement.CreateInstance(element);
+            XElement expectedSubelement = dynamicElement.Subelement;
+            Assert.AreEqual(expectedSubelement, subelement);
+
+            string value = dynamicElement.Subelement;
+            Assert.That(value, Is.EqualTo("value"));
+
+            Console.WriteLine("Subelement: {0}", expectedSubelement);
+            Console.WriteLine("Subelement value: {0}", value);
+        }
+
+        #endregion Accessing subelements
+    }
+}
---
+++ DynamicXml/DynamicXElement.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using System.Diagnostics.Contracts;
+using System.Dynamic;
+
+namespace DynamicXml
+{
+    /// <summary>
+    /// "Dynamic wrapper" around XElement.
+    /// </summary>
+    public class DynamicXElement : DynamicObject
+    {
+        //---------------------------------------------------------------------------------------//
+        // Private Fields
+        //---------------------------------------------------------------------------------------//
+        private XElement element;
+
+        //---------------------------------------------------------------------------------------//
+        // Construction, Destruction
+        //---------------------------------------------------------------------------------------//
+        private DynamicXElement(XElement element)
+        {
+            Contract.Requires(element != null);
+            this.element = element;
+        }
+
+        /// <summary>
+        /// Factory made intended usage more clear. We "should" use object of this class dynamically.
+        /// </summary>
+        public static dynamic CreateInstance(XElement element)
+        {
+            Contract.Requires(element != null);
+            return new DynamicXElement(element);
+        }
+
+        //---------------------------------------------------------------------------------------//
+        // System.Object overrides
+        //---------------------------------------------------------------------------------------//
+        
+        // All overrides for System.Object methods simply delegates they calls to underlying element
+
+        public override string ToString()
+        {
+            return element.ToString();
+        }
+        
+        public override bool Equals(object obj)
+        {
+            DynamicXElement rhs = obj as DynamicXElement;
+            if (ReferenceEquals(null, rhs))
+                return false;
+            return element == rhs.element;
+        }
+
+        public override int GetHashCode()
+        {
+            return element.GetHashCode();
+        }
+
+        //---------------------------------------------------------------------------------------//
+        // DynamicObject Overrides
+        //---------------------------------------------------------------------------------------//
+        
+        /// <summary>
+        /// Converting DynamicXElement to any other type means converting (or extracting) underlying
+        /// XElement.Value
+        /// </summary>
+        public override bool TryConvert(ConvertBinder binder, out object result)
+        {
+            // We're returning underlying XElement if required
+            if (binder.ReturnType == typeof(XElement))
+            {
+                result = element;
+                return true;
+            }
+
+            string underlyingValue = element.Value;
+            // If this conversion succeeded ok, otherwise we'll have exception
+            result = Convert.ChangeType(underlyingValue, binder.ReturnType);
+            return true;
+        }
+
+        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            string binderName = binder.Name;
+            Contract.Assume(binderName != null);
+            XElement subelement = element.Element(binderName);
+            if (subelement != null)
+            {
+                result = DynamicXElement.CreateInstance(subelement);
+                return true;
+            }
+
+            return base.TryGetMember(binder, out result);
+        }
+
+        [Pure]
+        public bool HasParent()
+        {
+            return element.Parent != null;
+        }
+
+        public DynamicXElement this[int idx]
+        {
+            get
+            {
+                
+                Contract.Requires(idx >= 0, "Index should be greater or equals to 0");
+                Contract.Requires(idx == 0 || HasParent(), "For non-zero index we should have parent element");
+
+                // For 0 index we returning current element);
+                if (idx == 0)
+                    return this;
+                
+                // For non-zero index we'll return appropriate peer.
+                // We should take parent and then access to appropriate child element
+                var parent = element.Parent;
+                Contract.Assume(parent != null);
+                var elements = parent.Elements().ToArray();
+                return CreateInstance(elements[idx]);
+            }
+        }
+    }
+}
